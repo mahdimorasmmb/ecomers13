41e89d6e5be6a404da2a4a188a87272331b58e4d
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2023-06-26 03:39:32 +0330

    fix api url

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
-const dns = require('dns')
-
-dns.setDefaultResultOrder("ipv4first");
 const nextConfig = {
   disableManifest: true,
   cacheBust: true,
   env: {
     NEXTAUTH_SECRET: "mahdimoras",
-    API_URL: "http://127.0.0.1:3000",
+    API_URL: "http://localhost:3000",
     DB_URL:
       "mongodb+srv://mahdimoras:zSzXyKpLob8uifNd@cluster0.8r5zn0r.mongodb.net/?retryWrites=true&w=majority",
   },
-
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
   },
